d9f8bfe2e0fa6e48abff4915a1a3ce015785c789
StephenBeckstead <stpcryptosocks@gmail.com>
2023-04-19 11:38:21 -0600

    ez money

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade percentage?");
+
+        string input = Console.ReadLine(); 
+        int grade = int.Parse(input); 
+
+        string letter = ""; 
+
+        if (grade >= 90) {
+            letter = "a";
+        }
+        else if (grade >= 80) {
+            letter = "b";
+        }
+        else if (grade >= 70) {
+            letter = "c";
+        }
+        else if (grade >= 60) {
+            letter = "d";
+        }
+        else {
+            letter = "f";
+        }
+
+        Console.WriteLine($"Your grade is: {letter}"); 
+
+        if (grade >= 70) {
+            Console.WriteLine("Congratz");
+        }
+        else {
+            Console.WriteLine("Maybe Next time");
+        }
     }
 }
