684f824f0ac199e6a2e7e5cc08796ab74357be10
StephenBeckstead <stpcryptosocks@gmail.com>
2023-04-24 23:33:56 -0600

    nice

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random(); 
+        int rand = randomGenerator.Next(1,100); 
+
+        int guess = -1; 
+
+        while (guess != rand) {
+            Console.WriteLine("What is your guess"); 
+            string notguess = Console.ReadLine(); 
+            guess = int.Parse(notguess); 
+
+            if (rand > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (rand < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+
+        }
+
+
+
     }
 }
