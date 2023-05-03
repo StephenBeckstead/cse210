0eb921a4e3fd89a329f3cfece5d7eb60801c0333
StephenBeckstead <stpcryptosocks@gmail.com>
2023-05-02 20:11:32 -0600

    nice

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job(); 
+        job1._jobTitle = "Fork Lifter"; 
+        job1._company = "Deseret Mill";
+        job1._startYear = 1; 
+        job1._endYear = 2; 
+
+        Job job2 = new Job(); 
+        job2._jobTitle = "Foamer"; 
+        job2._company = "Harvest Right";
+        job2._startYear = 1; 
+        job2._endYear = 2; 
+
+        Resume myResume = new Resume(); 
+        myResume._name = "Stephen Becktead"; 
+
+        myResume._jobs.Add(job1); 
+        myResume._jobs.Add(job2); 
+
+        myResume.Display();
     }
 }
