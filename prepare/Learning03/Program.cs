1db36d649811bff1af44c5cc6db6a9955d7c0c5f
StephenBeckstead <stpcryptosocks@gmail.com>
2023-05-17 20:31:49 -0600

    ye

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction fract = new Fraction(); 
+        Console.WriteLine(fract.GetFractionString());
+        Console.WriteLine(fract.GetDecimalValue()); 
+
+        Fraction fract1 = new Fraction(1); 
+        Console.WriteLine(fract1.GetFractionString());
+        Console.WriteLine(fract1.GetDecimalValue()); 
+
+        Fraction fract2 = new Fraction(2, 5); 
+        Console.WriteLine(fract2.GetFractionString());
+        Console.WriteLine(fract2.GetDecimalValue()); 
+
+        Fraction fract3 = new Fraction(7, 9); 
+        Console.WriteLine(fract3.GetFractionString());
+        Console.WriteLine(fract3.GetDecimalValue()); 
     }
 }
